aea43fc317ff2b31982ab2b256491719f346d789
FreddyAquinoPortes <frequipo2001@gmail.com>
2024-09-23 19:58:08 -0400

    Map screen update v1.2, blue spot added

--- app/src/main/java/com/example/melapp/Screens/PrincipalScreen.kt
+++ app/src/main/java/com/example/melapp/Screens/PrincipalScreen.kt
@@ -1,16 +1,19 @@
 // MapScreen.kt
 package com.example.melapp.Screens
 
+import android.Manifest
+import android.content.pm.PackageManager
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.core.app.ActivityCompat
 import com.google.maps.android.compose.GoogleMap
 import com.google.maps.android.compose.rememberCameraPositionState
 import com.example.melapp.utils.handleLocationPermissionAndMoveCamera
 import com.google.android.gms.maps.model.CameraPosition
 import com.google.android.gms.maps.model.LatLng
+import com.google.maps.android.compose.MapProperties
 
 @Composable
 fun MapScreen() {
@@ -24,16 +27,27 @@ fun MapScreen() {
         position = initialPosition
     }
 
-    // Llamar a la función para manejar los permisos de ubicación y mover la cámara
+    // Variable to control if the My Location layer is enabled
+    var myLocationEnabled by remember { mutableStateOf(false) }
+
+    // Check if the app has location permissions
+    if (ActivityCompat.checkSelfPermission(context, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED ||
+        ActivityCompat.checkSelfPermission(context, Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED
+    ) {
+        myLocationEnabled = true
+    }
+
+    // Call the function to handle location permissions and move the camera
     handleLocationPermissionAndMoveCamera(
         context = context,
         cameraPositionState = cameraPositionState
     )
 
-    // Mostrar el mapa
+    // Display the map
     GoogleMap(
         modifier = Modifier.fillMaxSize(),
-        cameraPositionState = cameraPositionState
+        cameraPositionState = cameraPositionState,
+        properties = MapProperties(isMyLocationEnabled = myLocationEnabled)
     )
 }
 
