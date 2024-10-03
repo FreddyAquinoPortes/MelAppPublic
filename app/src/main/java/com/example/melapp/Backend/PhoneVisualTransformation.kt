5984a85286891bc56a3ea77147ca3c63b58fd7ea
RahnyaNB <rahnya_6@hotmail.com>
2024-10-03 14:05:55 -0400

    Commit to dev of login solution

--- app/src/main/java/com/example/melapp/Backend/PhoneVisualTransformation.kt
+++ app/src/main/java/com/example/melapp/Backend/PhoneVisualTransformation.kt
@@ -5,17 +5,35 @@ import androidx.compose.ui.text.input.OffsetMapping
 import androidx.compose.ui.text.input.TransformedText
 import androidx.compose.ui.text.input.VisualTransformation
 
-// Transformación visual personalizada para el número de teléfono
 class PhoneVisualTransformation : VisualTransformation {
     override fun filter(text: AnnotatedString): TransformedText {
-        val trimmed = if (text.text.length >= 10) text.text.substring(0, 10) else text.text
-        val out = StringBuilder()
+        val trimmed = if (text.text.length >= 10) text.text.substring(0..9) else text.text
+        var out = ""
         for (i in trimmed.indices) {
-            if (i == 0) out.append("(")
-            if (i == 3) out.append(") ")
-            if (i == 6) out.append("-")
-            out.append(trimmed[i])
+            if (i == 0) out += "("
+            out += trimmed[i]
+            if (i == 2) out += ") "
+            if (i == 5) out += "-"
         }
-        return TransformedText(AnnotatedString(out.toString()), OffsetMapping.Identity)
+
+        val phoneNumberOffsetTranslator = object : OffsetMapping {
+            override fun originalToTransformed(offset: Int): Int {
+                if (offset <= 0) return offset
+                if (offset <= 3) return offset + 1
+                if (offset <= 6) return offset + 3
+                if (offset <= 10) return offset + 4
+                return 14
+            }
+
+            override fun transformedToOriginal(offset: Int): Int {
+                if (offset <= 1) return offset
+                if (offset <= 4) return offset - 1
+                if (offset <= 9) return offset - 3
+                if (offset <= 14) return offset - 4
+                return 10
+            }
+        }
+
+        return TransformedText(AnnotatedString(out), phoneNumberOffsetTranslator)
     }
 }
